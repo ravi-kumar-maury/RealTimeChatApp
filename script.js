4d0c378b8dbca6c79c2553dbddac8d302003e339
ravi-kumar-maury <suzerainravi@gmail.com>
2021-05-11 00:38:33 +0530

    feature of informed messaging added

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 const socket = io('http://localhost:3000')
-socket.on('chat-message',data =>{
-    console.log(data)
+
+var message = document.getElementById('message-input');
+var input = document.getElementById('send-button');
+var container =document.getElementsByClassName('message-container')
+
+const name1 = prompt("New Guys ? please enter your name")
+
+appendMessage('You Joined')
+socket.emit('new-user',name1);
+
+
+socket.on('user-connected',name1=>{
+    appendMessage(`${name1} connected` )
 })
+
+socket.on('user-disconnected',name1=>{
+   appendMessage(`${name1} disconnected`)
+})
+
+
+socket.on('chat-message',data =>{
+    appendMessage(`${data.name}:${data.message}`);
+})
+
+container.addEventListener('submit',e=>{
+    e.preventDefault()
+    const m = message.value;
+    appendMessage(`You : ${m}`)
+    socket.emit('send-chat-message',m)
+    message.value=''
+    
+})
+
+
+
+function appendMessage(mess)
+{
+   const  newDiv = document.createElement('div');
+   newDiv.innerText = mess
+   document.append(newDiv)
+}
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express();
+
 const io = require('socket.io')(3000,{
     cors:{
         origin:"*",
     },
 });
 
+
+const users = {}
+
+
 io.on('connection',(socket)=>{
-    console.log("new user")
-    socket.emit('chat-message','hello world')
+
+    socket.on('new-user',name1=>{
+        users[socket.id]=name1;
+        socket.broadcast.emit('user-connected', name1)
+    
+
+    socket.on('send-chat-message',m=>{  //name also have to be published
+        socket.broadcast.emit(`${users[socket.id]} : ${m}`)
+    })
+        
+            
+        });
+    })
+
+io.on('disconnect',()=>{
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
 })
-console.log("runnig on port "+process.env.PORT)
+    console.log("new user")
+
 
